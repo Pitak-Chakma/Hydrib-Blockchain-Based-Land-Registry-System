459c52a3f3d797297bc3ce10874792536d7d4153
Mahir Faisal <mahirshowmik@gmail.com>
2025-08-27 23:19:41 +0600

    Buyer Property Edit
    Buyer Property Edit

--- buyer.py
+++ buyer.py
@@ -1,1 +1,16 @@
 print("Hello World")
+##edinting Buyer Property 
+# Basic Python Program
+
+# Taking user input
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# Printing a greeting message
+print(f"Hello {name}! You are {age} years old.")
+
+# A simple condition
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
